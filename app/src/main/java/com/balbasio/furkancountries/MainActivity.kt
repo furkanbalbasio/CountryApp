82b9a0c54a0987bf52b31eebb82c2418e314def8
furkanbalbasio <87570972+furkanbalbasio@users.noreply.github.com>
2022-05-11 12:59:57 +0300

    Countries App

---
+++ app/src/main/java/com/balbasio/furkancountries/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.balbasio.furkancountries
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
